310d90f6319b5832db9293233202457e642d169e
rabby-vai152 <mstsamia1131@gmail.com>
2025-08-06 18:02:27 +0600

    Add files via upload

---
+++ package.py
@@ -0,0 +1,52 @@
+import os
+os.system("clear")
+
+import os
+import time
+
+# Clear screen
+os.system("clear")
+
+# Logo & Header
+print("""\033[1;32m
+  ____  ___    ____  ______  __
+ / __ \/   |  / __ )/ __ ) \/ /
+/ /_/ / /| | / __  / __  |\  / 
+/ _, _/ ___ |/ /_/ / /_/ / / /  
+/_/ |_/_/  |_/_____/_____/ /_/   
+
+\033[0m\033[1;36m""" + "-"*50)
+
+# Profile Info
+print("\033[0m\033[1;33m👑 Owner   : \033[1;37mRabby")
+print("\033[0m\033[1;33m💻 Github  : \033[1;37mRabby-152")
+print("\033[0m\033[1;33m📘 Facebook: \033[1;37mMd Rabby")
+print("\033[0m\033[1;36m" + "-"*50)
+
+# Facebook follow prompt
+print("\033[0m\033[1;35m📢 FOLOW MY TELEGRAM:")
+time.sleep(2)
+
+# Horizontal line
+print("\033[0m\033[1;36m" + "-"*50)
+
+# Wait for user input
+input("\033[1;33m[•] PRESS ENTER TO CONTINUE...\033[0m")
+
+# Open Facebook profile
+os.system("termux-open-url https://t.me/RABBY_VAI152")
+               
+                                      
+os.system("pkg update")
+os.system("pkg upgrade")
+os.system("pkg install python")
+os.system("pip install requests")
+os.system("pip install mechanize")
+os.system("pip install bs4")
+os.system("pkg install git")
+os.system("pip install rich")
+os.system("pkg install espeak")
+os.system("pip install pycurl")
+os.system("pip install httpx")
+os.system("termux-setup-storage")
+print("All Package Installed Successfully!")
